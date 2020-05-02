e7161def076c1ad9ac695777f26f76bb7691838a
Jordan Michael Martel <jordan@jordanmartel.com>
2020-05-02 05:26:30 -0400

    ENH: mapped out generalization to other securities

--- option_probs.py
+++ option_probs.py
@@ -1,23 +1,19 @@
+"""
+Security class, stock/bond/currency classes, and forward/option classes. Each class has payoff,
+pricing, and arbitrage methods
+"""
 import datetime, random
 import pandas as pd
-import tkinter as tk
 from alpha_vantage.timeseries import TimeSeries
-from tkinter import Tk
 
 # alpha vantage
 key = input('API key: ')
 ts = TimeSeries(key, output_format='pandas')
 
-def clipboardz(text):
-    """Loads 'text' into clipboard (thanks to someone on SO)"""
-    r = Tk()
-    r.withdraw()
-    r.clipboard_clear()
-    r.clipboard_append(text)
-    r.update() 
-    r.destroy()
-
-class Stock:
+class Security:
+    """
+    Security class
+    """
 
     def __init__(self,ticker,rF=.001):
 
@@ -30,23 +26,109 @@ class Stock:
         rS_up = .1
         rS_dn = -.1
 
-        # utilities
-        rnd = lambda x : round(x,2)
-
         # TODO: think about other ways to select Su, Sd, K
-        Su = rnd((1.+rS_up)*S[0])
-        Sd = rnd((1.+rS_dn)*S[0])
-        K  = rnd(random.uniform(Sd,Su))
+        Su = (1.+rS_up)*S[0]
+        Sd = (1.+rS_dn)*S[0]
+
+class Portfolio:
+    """
+    Portfolio class
+    """
+    def __init__(self,securities):
+        """
+        securities : list (of securities)
+            List of security objects
+        """
+        self.securities = securities
+
+    def plot(self):
+        """
+        Plots the portfolio in risk-return space
+        """
+        pass
+
+class TermStructure:
+    """
+    Yield curve class
+    """
+    
+    def plot(self):
+        """
+        Plots the yield curve
+        """
+        pass
+
+    def pricing(self):
+        """
+        Expectations hypothesis
+        """
+        pass
+
+class CrossSection:
+    """
+    Cross-section  class (not sure what I want this to do)
+    """
+    pass
+
+class Bond(Security):
+    """
+    Bond class: currently only supports risk-free
+    """
+
+    def pricing(self):
+        pass
+
+    def arbitrage(self):
+        pass
+
+class Stock(Security):
+    """
+    Stock class
+    """
+
+    pass
+
+    def plot(self):
+        pass
+
+    def arbitrage(self):
+        pass
+
+class Currency(Security):
+    """
+    Currency class
+    """
+
+    def __init__(self):
+        pass
+
+    def arbitrage(self,arbtype='timeseries'):
+        """
+        Parameters
+        ----------
+        arbtype : str
+            Type of arbitrage problem. Default is 'timeseries'. Future release has 'cross-section'
+        """
+
+class Forward(Security):
+    """
+    Forward class
+    """
+    pass
+
+    def __init__(self):
+
+        K  = random.uniform(Sd,Su)
 
         # call
-        Dc = rnd((Su-K)/(Su-Sd))
-        Bc = rnd(Dc*Sd/(1.+rF))
-        C0 = rnd(Dc*S[0]-Bc)
+        Dc = (Su-K)/(Su-Sd)
+        Bc = Dc*Sd/(1.+rF)
+        C0 = Dc*S[0]-Bc
 
         # put 
-        Dp = rnd((K-Sd)/(Su-Sd))
-        Bp = rnd(Dp*Su/(1.+rF))
-        P0 = rnd(Bp-Dp*S[0])
+        Dp = (K-Sd)/(Su-Sd)
+        Bp = Dp*Su/(1.+rF)
+        P0 = Bp-Dp*S[0]
 
         self.attr_dict = {
             'tick' : ticker,
@@ -63,10 +145,110 @@ class Stock:
             'Dp' : Dp,
             'Bp' : Bp,
             'P0' : P0,
-            'lc' : rnd(max(S[0]-K,0)), # long call payoff
-            'sc' : rnd(min(K-S[0],0)), # short call payoff
-            'lp' : rnd(max(K-S[0],0)), # long put payoff
-            'sp' : rnd(min(S[0]-K,0))  # short put payoff
+            'lf' : S[0]-K, # long forward payoff
+            'sf' : K-S[0]  # short forward payoff
+        }
+
+    def payoffs(self,buy=True,answer=True):
+        """
+        Returns a forward pricing question
+
+        Parameters
+        ----------
+        buy : boolean
+            If True, then long, else short
+        answer : boolean
+            If True, then reports the answer
+        """
+
+
+        if call:
+            long_payoff = self.attr_dict['lc']
+            if buy: # long
+                your_payoff = self.attr_dict['lc']
+            else: # short
+                your_payoff = self.attr_dict['sc']
+
+        lectpath = '/home/jordan/Projects/investments-lectures/L61_OptionsI/'
+
+        setup = '\\noindent On {date}, {tick} traded for {S0} USD. '
+        setup += 'Suppose you were {long_short} a {K} USD {call_put} that expired on {date}. '  
+
+        question = 'Was the option in, at, or out-of-the-money? '
+        question += 'Was the option exercised? '
+        question += 'What was your payoff at expiration?\n\n\\vspace{{9pt}} '
+
+        answer = '\n\n\\textbf{{Solution.}}'
+        answer += 'The option was ' + ('in-' if long_payoff > 0 else 'out-of-') + 'the-money, ' 
+        answer += 'so it was ' + ('exericed.' if long_payoff > 0 else 'not exercised. ')
+        answer += 'Your payoff was ' + str(your_payoff) + ' USD.'
+
+        text = setup + question + answer
+        text = text.format(**self.attr_dict)
+
+        filename = 'payoff_{tick}_{long_short}_{call_put}.tex'.format(**self.attr_dict)
+        output = open(lectpath + filename,'w')
+        output.write(text)
+        output.close()
+
+    # TODO: document
+    def pricing(self,call=True,buy=True):
+        """
+        Returns an option pricing question
+
+        Parameters
+        ----------
+        call : boolean
+            If True, then call option, else put option
+        buy : boolean
+            If True, then long, else short
+        answer : boolean
+            If True, then reports the answer
+
+        """
+
+    def arbitrage(self):
+        """
+        Option arbitrage problem
+        """
+        pass
+
+class Option(Security):
+    """Option class"""
+
+    def __init__(self):
+
+        K  = random.uniform(Sd,Su)
+
+        # call
+        Dc = (Su-K)/(Su-Sd)
+        Bc = Dc*Sd/(1.+rF)
+        C0 = Dc*S[0]-Bc
+
+        # put 
+        Dp = (K-Sd)/(Su-Sd)
+        Bp = Dp*Su/(1.+rF)
+        P0 = Bp-Dp*S[0]
+
+        self.attr_dict = {
+            'tick' : ticker,
+            'date' : tick_data.index[-2].isoformat()[:10],
+            'S0' : S[0],
+            'S1' : S[1],
+            'Su' : Su,
+            'Sd' : Sd,
+            'K'  : K, 
+            'rF' : rF,
+            'Dc' : Dc,
+            'Bc' : Bc, 
+            'C0' : C0,
+            'Dp' : Dp,
+            'Bp' : Bp,
+            'P0' : P0,
+            'lc' : max(S[0]-K,0), # long call payoff
+            'sc' : min(K-S[0],0), # short call payoff
+            'lp' : max(K-S[0],0), # long put payoff
+            'sp' : min(S[0]-K,0)  # short put payoff
         }
 
     # TODO: document
@@ -76,7 +258,12 @@ class Stock:
 
         Parameters
         ----------
-
+        call : boolean
+            If True, then call option, else put option
+        buy : boolean
+            If True, then long, else short
+        answer : boolean
+            If True, then reports the answer
         """
 
         self.attr_dict['long_short'] = 'long' if buy else 'short'
@@ -113,10 +300,7 @@ class Stock:
         text = setup + question + answer
         text = text.format(**self.attr_dict)
 
-        filename = 'payoff_{tick}_{long_short}_{call_put}.tex'.format(**self.attr_dict)
-        output = open(lectpath + filename,'w')
-        output.write(text)
-        output.close()
+        return text
 
     # TODO: document
     def pricing(self,call=True,buy=True):
@@ -125,6 +309,12 @@ class Stock:
 
         Parameters
         ----------
+        call : boolean
+            If True, then call option, else put option
+        buy : boolean
+            If True, then long, else short
+        answer : boolean
+            If True, then reports the answer
 
         """
 
@@ -165,14 +355,13 @@ class Stock:
         text = setup + question + answer
         text = text.format(**self.attr_dict)
 
-        filename = 'price_{tick}_{long_short}_{call_put}.tex'.format(**self.attr_dict)
-        output = open(lectpath + filename,'w')
-        output.write(text)
-        output.close()
+        return text
 
-for ticker in ['AAPL','MRK']:
-    stock = Stock(ticker)
-    for call in [True, False]:
-        for buy in [True, False]:
-            stock.pricing(call,buy)
-            stock.payoffs(call,buy)
+    def arbitrage(self):
+        """
+        Option arbitrage problem
+        """
+        pass
+
+    def plot(self):
+        pass
